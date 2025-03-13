fde807260e8b274c1b1f069aeb8096960eef1215
Yohann <73314529+yohann69@users.noreply.github.com>
2025-03-13 16:56:00 +0100

    Update main.go

--- main.go
+++ main.go
@@ -160,7 +160,7 @@ func main() {
 	api.Get("/files", jwtMiddleware, listUserFiles)
 	api.Delete("/files/:filename", jwtMiddleware, deleteFile)
 
-	api.Get("all-files", jwtMiddleware, listAllFiles)
+	api.Get("/all-files", jwtMiddleware, listAllFiles)
 
 	log.Fatal(app.Listen(":3000"))
 }
