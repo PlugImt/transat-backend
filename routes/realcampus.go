d9a67cbee000da3fd0b7ebd79a91dc2417851896
Maxime Bodin <maxime.bodin@mailo.com>
2025-04-12 22:37:51 +0200

    Fixing middleware issue in realcampus routes.

--- routes/realcampus.go
+++ routes/realcampus.go
@@ -2,40 +2,36 @@ package routes
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
-	"os"
-	"strings"
 
+	"Transat_2.0_Backend/middlewares"
 	"Transat_2.0_Backend/realcampus/friendships"
 	"Transat_2.0_Backend/realcampus/posts"
 	"Transat_2.0_Backend/realcampus/users"
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v5"
 )
 
 // SetupRealCampusRoutes configures all the routes for RealCampus feature
 func SetupRealCampusRoutes(router fiber.Router, db *sql.DB) {
 	// Create a group for RealCampus routes.
-	realcampus := router.Group("/realcampus")
+	realcampus := router.Group("/realcampus", middlewares.JWTMiddleware)
 
 	// Post-related routes.
 	// Group all posts routes under /api/posts.
-	postsGroup := realcampus.Group("/posts")
-	postsGroup.Get("/today", jwtMiddleware, posts.GetUserTodayPosts(db))
-	postsGroup.Post("/", jwtMiddleware, posts.CreatePost(db))
+	postsGroup := realcampus.Group("/posts", middlewares.JWTMiddleware)
+	postsGroup.Get("/today", posts.GetUserTodayPosts(db))
+	postsGroup.Post("/", posts.CreatePost(db))
 
 	// Friendship-related routes.
 	// Group all friendship routes under /api/friendships.
-	friendshipsGroup := realcampus.Group("/friendships")
-	friendshipsGroup.Get("/", jwtMiddleware, friendships.GetUserFriendships(db))
-	friendshipsGroup.Post("/send", jwtMiddleware, friendships.SendFriendRequest(db))
-	friendshipsGroup.Post("/reject", jwtMiddleware, friendships.RejectFriendRequest(db))
-	friendshipsGroup.Post("/accept", jwtMiddleware, friendships.AcceptFriendRequest(db))
-	friendshipsGroup.Post("/remove", jwtMiddleware, friendships.RemoveFriend(db))
-	friendshipsGroup.Post("/cancel", jwtMiddleware, friendships.CancelFriendRequest(db))
+	friendshipsGroup := realcampus.Group("/friendships", middlewares.JWTMiddleware)
+	friendshipsGroup.Get("/", friendships.GetUserFriendships(db))
+	friendshipsGroup.Post("/send", friendships.SendFriendRequest(db))
+	friendshipsGroup.Post("/reject", friendships.RejectFriendRequest(db))
+	friendshipsGroup.Post("/accept", friendships.AcceptFriendRequest(db))
+	friendshipsGroup.Post("/remove", friendships.RemoveFriend(db))
+	friendshipsGroup.Post("/cancel", friendships.CancelFriendRequest(db))
 
-	router.Get("/users/search", jwtMiddleware, users.SearchUsers(db))
+	router.Get("/users/search", middlewares.JWTMiddleware, users.SearchUsers(db))
 
 	/*
 			realcampus.Get("/posts/friends", handlers.GetFriendPosts(db))
@@ -45,71 +41,4 @@ func SetupRealCampusRoutes(router fiber.Router, db *sql.DB) {
 			realcampus.Get("/posts/:postID/reactions", handlers.GetPostReactions(db))
 			realcampus.Post("/posts/:postID/reactions", handlers.AddReaction(db))
 	*/
-}
-
-/*
-*
-IMPORTANT : AS security.go SHOULD BE PLACED IN A DEDICATED PACKAGE AND THE ENTIRE PROJECT
-ORGANIZATION SHOULD BE REFACTORED, THE CODE BELOW SHOULD BE REMOVED AFTER REFACTOR. WHILE
-REFACTOR IS NOT DONE, CODE BELOW IS NECESSARY.
-*/
-func jwtMiddleware(c *fiber.Ctx) error {
-	authHeader := c.Get("Authorization")
-
-	log.Println("╔======== 📧 JWT Middleware 📧 ========╗")
-
-	if authHeader == "" {
-		log.Println("║ 💥 Missing token")
-		log.Println("╚=======================================╝")
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Missing token"})
-	}
-
-	tokenString := authHeader
-	if strings.HasPrefix(authHeader, "Bearer ") {
-		tokenString = authHeader[7:]
-	}
-
-	token, err := validateJWT(tokenString)
-	if err != nil {
-		log.Println("║ 💥 Invalid token: ", err)
-		log.Println("╚=======================================╝")
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token"})
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		log.Println("║ 💥 Invalid claims")
-		log.Println("╚=======================================╝")
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid claims"})
-	}
-
-	c.Locals("email", claims["email"])
-
-	log.Println("║ ✅ Token is valid")
-	log.Println("║ 📧 Email: ", claims["email"])
-	log.Println("╚=======================================╝")
-
-	return c.Next()
-}
-
-var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
-
-func validateJWT(tokenString string) (*jwt.Token, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Ensure the signing method is correct
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return jwtSecret, nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	if !token.Valid {
-		return nil, fmt.Errorf("invalid token")
-	}
-
-	return token, nil
 }
