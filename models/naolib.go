954c465b966c49298b7dc1b98409656252fe7809
Lucie Delestre <me@luclu7.fr>
2025-05-23 23:04:19 +0200

    nettoyage

--- models/naolib.go
+++ models/naolib.go
@@ -1,29 +1,5 @@
 package models
 
-// Departure représente un départ de bus
-type Departure struct {
-	Sens          int    `json:"sens"`
-	Terminus      string `json:"terminus"`
-	InfoTrafic    bool   `json:"infotrafic"`
-	Temps         string `json:"temps"`
-	DernierDepart string `json:"dernierDepart"`
-	TempsReel     string `json:"tempsReel"`
-	Ligne         Ligne  `json:"ligne"`
-	Arret         Arret  `json:"arret"`
-}
-
-// Ligne représente les informations sur la ligne de bus
-type Ligne struct {
-	NumLigne  string `json:"numLigne"`
-	TypeLigne int    `json:"typeLigne"`
-}
-
-// Arret représente les informations sur l'arrêt de bus
-type Arret struct {
-	CodeArret string `json:"codeArret"`
-}
-
-// custom
 type DepartureDirection struct {
 	Direction  string               `json:"direction"`
 	Departures []MonitoredStopVisit `json:"departures"`
