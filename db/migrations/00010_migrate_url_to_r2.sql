a8ddcd795d96dda5634eed408f23a9813309d65c
Lucie Delestre <me@luclu7.fr>
2025-06-11 15:57:56 +0200

    fix url

--- db/migrations/00010_migrate_url_to_r2.sql
+++ db/migrations/00010_migrate_url_to_r2.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-UPDATE newf SET profile_picture = REPLACE(profile_picture, 'https://transat.destimt.fr/api/data/', 'https://assets.dev.transat.dev/') WHERE profile_picture LIKE 'https%';
+UPDATE newf SET profile_picture = REPLACE(profile_picture, 'https://transat.destimt.fr/api/data/', 'https://assets.prod.transat.dev/') WHERE profile_picture LIKE 'https%';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-UPDATE newf SET profile_picture = REPLACE(profile_picture, 'https://assets.dev.transat.dev/', 'https://transat.destimt.fr/api/data/') WHERE profile_picture LIKE 'https%';
+UPDATE newf SET profile_picture = REPLACE(profile_picture, 'https://assets.prod.transat.dev/', 'https://transat.destimt.fr/api/data/') WHERE profile_picture LIKE 'https%';
 -- +goose StatementEnd
