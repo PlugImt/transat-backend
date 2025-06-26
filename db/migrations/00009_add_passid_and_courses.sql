a093594f35cdeccbfbbb035db10b6561022c9493
Maxime Bodin <maxime.bodin@mailo.com>
2025-06-26 10:43:33 +0200

    Making start_time and end_time TIMESTAMP as we need the time not just the date. Addding a necessary unique constraint on (user_email, title, start_time, end_time) to handle duplicate.

--- db/migrations/00009_add_passid_and_courses.sql
+++ db/migrations/00009_add_passid_and_courses.sql
@@ -6,13 +6,17 @@ CREATE TABLE courses (
     id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     title VARCHAR(255) NOT NULL,
-    start_time DATE NOT NULL,
-    end_time DATE NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
     teacher VARCHAR(255),
     room VARCHAR(255),
     "group" VARCHAR(255),
     user_email VARCHAR(255) REFERENCES newf(email) ON DELETE CASCADE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    -- UNIQUE constraint to define what a "duplicate" course is.
+    -- This is essential for the ON CONFLICT clause to work.
+    UNIQUE (user_email, title, start_time, end_time)
 );
 -- +goose StatementEnd
 
