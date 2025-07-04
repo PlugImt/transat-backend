4ec8cb42b5f824afe79d7a2229936e721f9fab90
Mistourr <alexisyon42@gmail.com>
2025-07-04 16:23:31 +0200

    add homework model

---
+++ models/homework.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Homework struct {
+	ID          int       `json:"id"`
+	Author      string    `json:"author"`
+	CourseName  string    `json:"course_name"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Deadline    string    `json:"deadline`
+	Done        bool      `json:"done"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
