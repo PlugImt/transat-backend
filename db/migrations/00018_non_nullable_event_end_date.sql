d3470985c60b010681e3f1a8a639046dc6baac19
Lucie Delestre <me@luclu7.fr>
2025-08-25 00:10:53 +0200

    le lapin a raison

--- db/migrations/00018_non_nullable_event_end_date.sql
+++ db/migrations/00018_non_nullable_event_end_date.sql
@@ -1,11 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE events
+    ADD CONSTRAINT events_end_date_gte_start_date
+    CHECK (end_date >= start_date);
+ALTER TABLE events
     ALTER COLUMN end_date SET NOT NULL;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+ALTER TABLE events
+    DROP CONSTRAINT events_end_date_gte_start_date;
 ALTER TABLE events
     ALTER COLUMN end_date DROP NOT NULL;
 -- +goose StatementEnd
